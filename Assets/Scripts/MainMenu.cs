506629eb36f1d135d54252dcd3fe6a67180ec687
nahuelpignataro <nahuel_q@hotmail.es>
2026-06-09 13:45:12 -0300

    Añade escenas, UI, TextMeshPro y scripts de menú
    Añade nuevas escenas y assets de Timeline junto con TimelineSettings.asset. Añade sprites de UI/splash y recursos/shaders de TextMesh Pro. Incluye los scripts MainMenu.cs y TimeLineChargeGame.cs junto con sus archivos meta. Actualiza ProjectSettings (EditorBuildSettings.asset, ProjectSettings.asset, TagManager.asset) para reflejar los nuevos assets

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("History");
+    }
+    public void Credits()
+    {
+
+    }
+    public void Exit()
+    {
+        Application.Quit();
+    }
+}
